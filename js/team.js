478476b6294f23a0ae0e8b671da9ca2f015f3c14
Tori <toriya.levitskaya@mail.ru>
2022-09-14 02:15:14 +0300

    project created

---
+++ js/team.js
@@ -0,0 +1,62 @@
+(() => {
+	let teamSwiper = new Swiper(".js-team-swiper", {
+		slidesPerView: 1,
+		grid: {
+			rows: 1,
+			fill: "row"
+		},
+		spaceBetween: 20,
+
+		navigation: {
+			nextEl: ".js-team-next",
+			prevEl: ".js-team-prev",
+			disabledClass: "nav-btn--disabled"
+		},
+
+		breakpoints: {
+			421: {
+				slidesPerView: 2,
+				slidesPerGroup: 2,
+				spaceBetween: 30
+			},
+
+			1280: {
+				slidesPerView: 3,
+				slidesPerGroup: 3,
+				spaceBetween: 30
+			}
+		},
+
+		a11y: false,
+		keyboard: {
+			enabled: true,
+			onlyInViewport: true
+		}, // можно управлять с клавиатуры стрелками влево/вправо
+
+		// Дальнейшие надстройки делают слайды вне области видимости не фокусируемыми
+		watchSlidesProgress: true,
+		watchSlidesVisibility: true,
+		slideVisibleClass: "slide-visible",
+
+		on: {
+			init: function () {
+				this.slides.forEach((slide) => {
+					if (!slide.classList.contains("slide-visible")) {
+						slide.tabIndex = "-1";
+					} else {
+						slide.tabIndex = "";
+					}
+				});
+			},
+			slideChange: function () {
+				this.slides.forEach((slide) => {
+					if (!slide.classList.contains("slide-visible")) {
+						slide.tabIndex = "-1";
+					} else {
+						slide.tabIndex = "";
+					}
+				});
+			}
+		}
+	});
+})();
